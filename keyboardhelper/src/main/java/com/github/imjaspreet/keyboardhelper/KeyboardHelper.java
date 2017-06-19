abfc447e67d6a9eb22033d2b57e381d32bfd6dbc
jaspreet singh <jaspreet.singh70077@gmail.com>
2017-06-19 22:30:18 +0530

    add new function and fixes

--- keyboardhelper/src/main/java/com/github/imjaspreet/keyboardhelper/KeyboardHelper.java
+++ keyboardhelper/src/main/java/com/github/imjaspreet/keyboardhelper/KeyboardHelper.java
@@ -1,11 +1,13 @@
 package com.github.imjaspreet.keyboardhelper;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Build;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 
 import java.lang.ref.WeakReference;
 
@@ -92,5 +94,35 @@ public class KeyboardHelper {
         void onKeyboardShown(int keyboardSize);
 
         void onKeyboardClosed();
+    }
+
+    public static void showKeyboard(Activity activity) {
+        InputMethodManager inputManager = (InputMethodManager) activity.
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (inputManager != null) {
+            inputManager.toggleSoftInput(0, InputMethodManager.SHOW_IMPLICIT);
+        }
+    }
+
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager inputManager = (InputMethodManager) activity.
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (activity.getCurrentFocus() != null) {
+            inputManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(),
+                    InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+    }
+
+    public static void hideKeyboard(View view) {
+        InputMethodManager inputManager = (InputMethodManager) view.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    public void hideKeyboardForcefully() {
+        activityRef.get().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+
+    public void showKeyboardForcefully() {
+        activityRef.get().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 }
